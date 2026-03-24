adf60ac5897eb05577992b02749430fdd3409465
Surendran M <surendranmurugesan2@gmail.com>
2026-03-24 08:49:35 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args) {
+        System.out.print("Hello World!\n");
+        byte v1=127;
+        byte v2=-128;
+        System.out.print(v1+","+v2);
+        boolean b1=false;
+        boolean b2=true;
+        System.out.print("\nValue of boolean is " + b1 + "," + b2);
+    }
+}
---
+++ Primitive.java
@@ -0,0 +1,21 @@
+public class Primitive {
+    public static void main(String[] args) {
+        float f=10.5f;
+        System.out.print("Value of float is " + f);
+        double d=1444040.123456789;
+        System.out.print("\nValue of double is " + d);
+        char c='A';
+        System.out.print("\nValue of char is " + c);
+        long l=1234567890123456789L;
+        System.out.print("\nValue of long is " + l);
+        short s = 1000;
+        System.out.print("\nValue of short is " + s);
+        System.out.print("\nRange of short: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        int i = 100000;
+        System.out.print("\nValue of int is " + i);
+        System.out.print("\nRange of int: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.print("\nRange of long: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        
+    }
+    
+}
