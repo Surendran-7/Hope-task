cd04b0a195a71ca70ff7e682d7fb8599575982b0
Surendran M <surendranmurugesan2@gmail.com>
2026-03-31 06:05:50 +0530

    Add files via upload

---
+++ InterfaceDiamondPattern.java
@@ -0,0 +1,30 @@
+ 
+package DAY_9.Task;
+class A{
+    void display(){
+        System.out.println("I am in class A");
+    }
+}
+interface B{
+    void display();
+}
+interface C{
+    void display();
+}
+class D extends A implements B,C{
+    public void display(){
+        System.out.println("I am in class D");
+    }
+    public void displayA(){
+        super.display();
+    }
+}
+public class InterfaceDiamondPattern {
+    public static void main(String[] args) {
+        D d=new D();
+        d.displayA();
+        d.display();
+    }
+    
+}
+ 
